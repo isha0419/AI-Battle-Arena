0f5fcf37815de7e253817099d72ab6aa9a378a2c
isha0419 <isha01092004@gmail.com>
2026-07-11 01:26:25 +0530

    Update CORS configuration for frontend deployment

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:[
+        "http://localhost:5173", 
+        "https://ai-battle-arena-backend-0ezw.onrender.com/"
+    ],
     methods:["GET","POST"],
     credentials:true
 }))
